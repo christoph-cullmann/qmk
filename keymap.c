0777da11588a9ccd08b451f662e47d53180b99e3
Christoph Cullmann <cullmann@kde.org>
2022-10-07 17:25:38 +0200

    esc and enter back to the generic positions

--- keymap.c
+++ keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
  * ,-----------------------------------------------------------------------------------.
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Esc |
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -41,8 +41,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,    KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_LCTL,  LT(_NAV, KC_A),    RALT_T(KC_S),    LALT_T(KC_D),     LSFT_T(KC_F),    KC_G,    KC_H,    RSFT_T(KC_J),   LALT_T(KC_K),    RALT_T(KC_L),    LT(_NAV, KC_SCLN),     RCTL_T(KC_QUOT),
-    LSFT_T(KC_ESC),  KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,    KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  RSFT_T(KC_ESC),
+    LCTL_T(KC_ESC),  LT(_NAV, KC_A),    RALT_T(KC_S),    LALT_T(KC_D),     LSFT_T(KC_F),    KC_G,    KC_H,    RSFT_T(KC_J),   LALT_T(KC_K),    RALT_T(KC_L),    LT(_NAV, KC_SCLN),     RCTL_T(KC_QUOT),
+    KC_LSFT,  KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,    KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  RSFT_T(KC_ENT),
     KC_LCTL, KC_LGUI, KC_LALT, MO(_CMD),   MO(_LOWER),  KC_SPC,    KC_ENT,   MO(_RAISE),   MO(_FN),   KC_RALT, KC_RGUI, KC_RCTL
 ),
 
