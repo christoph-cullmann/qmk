5ddf2fc623ffece40d72c75872ef68c0a13c8ad8
Christoph Cullmann <cullmann@kde.org>
2022-10-21 15:31:55 +0200

    more layer switches

--- keymap.c
+++ keymap.c
@@ -78,36 +78,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_planck_grid(
     KC_Q,         KC_W,         KC_E,         KC_R,             KC_T,             XXXXXXX, XXXXXXX, KC_Y,            KC_U,              KC_I,         KC_O,           KC_P,
-    LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F),     KC_G,             XXXXXXX, XXXXXXX, KC_H,            RSFT_T(KC_J),      RCTL_T(KC_K), LALT_T(KC_L),   RGUI_T(KC_QUOT),
-    KC_Z,         RALT_T(KC_X), KC_C,         KC_V,             KC_B,             XXXXXXX, XXXXXXX, KC_N,            KC_M,              KC_COMM,      RALT_T(KC_DOT), KC_SLSH,
+    LT(_NUM, KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F),     KC_G,             XXXXXXX, XXXXXXX, KC_H,            RSFT_T(KC_J),      RCTL_T(KC_K), LALT_T(KC_L),   LT(_NAV, KC_QUOT),
+    LT(_SYM, KC_Z),         RALT_T(KC_X), LGUI_T(KC_C), KC_V,             KC_B,             XXXXXXX, XXXXXXX, KC_N,            KC_M,              RGUI_T(KC_COMM), RALT_T(KC_DOT), LT(_FN, KC_SLSH),
     XXXXXXX,      XXXXXXX,      XXXXXXX,      LT(_SYM, KC_TAB), LT(_NUM, KC_SPC), XXXXXXX, XXXXXXX, LT(_FN, KC_ENT), LT(_NAV, KC_BSPC), XXXXXXX,      XXXXXXX,        XXXXXXX
 ),
 
 [_SYM] = LAYOUT_planck_grid(
     KC_ESC,       C(KC_X),      C(KC_C),      C(KC_V),          XXXXXXX,          XXXXXXX, XXXXXXX, KC_LCBR,         KC_AMPR,           KC_ASTR,      KC_LPRN,        KC_RCBR,
-    KC_LGUI,      KC_LALT,      KC_LCTL,      KC_LSFT,          XXXXXXX,          XXXXXXX, XXXXXXX, KC_PLUS,         KC_DLR,            KC_PERC,      KC_CIRC,        KC_COLN,
-    XXXXXXX,      KC_RALT,      XXXXXXX,      XXXXXXX,          XXXXXXX,          XXXXXXX, XXXXXXX, KC_PIPE,         KC_EXLM,           KC_AT,        KC_HASH,        KC_TILD,
+    XXXXXXX,      KC_LALT,      KC_LCTL,      KC_LSFT,          XXXXXXX,          XXXXXXX, XXXXXXX, KC_PLUS,         KC_DLR,            KC_PERC,      KC_CIRC,        KC_COLN,
+    XXXXXXX,      KC_RALT,      KC_LGUI,      XXXXXXX,          XXXXXXX,          XXXXXXX, XXXXXXX, KC_PIPE,         KC_EXLM,           KC_AT,        KC_HASH,        KC_TILD,
     XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_TAB,           KC_SPC,           XXXXXXX, XXXXXXX, KC_UNDS,         KC_RPRN,           XXXXXXX,      XXXXXXX,        XXXXXXX
 ),
 
 [_NUM] = LAYOUT_planck_grid(
     KC_ESC,       C(KC_X),      C(KC_C),      C(KC_V),          XXXXXXX,          XXXXXXX, XXXXXXX, KC_LBRC,         KC_7,              KC_8,         KC_9,           KC_RBRC,
-    KC_LGUI,      KC_LALT,      KC_LCTL,      KC_LSFT,          XXXXXXX,          XXXXXXX, XXXXXXX, KC_EQL,          KC_4,              KC_5,         KC_6,           KC_SCLN,
-    XXXXXXX,      KC_RALT,      XXXXXXX,      XXXXXXX,          XXXXXXX,          XXXXXXX, XXXXXXX, KC_BSLS,         KC_1,              KC_2,         KC_3,           KC_GRV,
+    XXXXXXX,      KC_LALT,      KC_LCTL,      KC_LSFT,          XXXXXXX,          XXXXXXX, XXXXXXX, KC_EQL,          KC_4,              KC_5,         KC_6,           KC_SCLN,
+    XXXXXXX,      KC_RALT,      KC_LGUI,      XXXXXXX,          XXXXXXX,          XXXXXXX, XXXXXXX, KC_BSLS,         KC_1,              KC_2,         KC_3,           KC_GRV,
     XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_TAB,           KC_SPC,           XXXXXXX, XXXXXXX, KC_MINS,         KC_0,              XXXXXXX,      XXXXXXX,        XXXXXXX
 ),
 
 [_FN] = LAYOUT_planck_grid(
     KC_F12,       KC_F7,        KC_F8,        KC_F9,            KC_PSCR,          XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX,           XXXXXXX,      XXXXXXX,        QK_BOOT,
-    KC_F11,       KC_F4,        KC_F5,        KC_F6,            XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX,         KC_RSFT,           KC_RCTL,      KC_LALT,        KC_RGUI,
-    KC_F10,       KC_F1,        KC_F2,        KC_F3,            XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX,           XXXXXXX,      KC_RALT,        XXXXXXX,
+    KC_F11,       KC_F4,        KC_F5,        KC_F6,            XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX,         KC_RSFT,           KC_RCTL,      KC_LALT,        XXXXXXX,
+    KC_F10,       KC_F1,        KC_F2,        KC_F3,            XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX,           KC_RGUI,      KC_RALT,        XXXXXXX,
     XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_TAB,           KC_SPC,           XXXXXXX, XXXXXXX, KC_ENT,          KC_BSPC,           XXXXXXX,      XXXXXXX,        XXXXXXX
 ),
 
 [_NAV] = LAYOUT_planck_grid(
     KC_PGUP,      KC_HOME,      KC_UP,        KC_END,           KC_PGUP,          XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX,           XXXXXXX,      XXXXXXX,        QK_BOOT,
-    KC_PGDN,      KC_LEFT,      KC_DOWN,      KC_RGHT,          KC_PGDN,          XXXXXXX, XXXXXXX, XXXXXXX,         KC_RSFT,           KC_RCTL,      KC_LALT,        KC_RGUI,
-    XXXXXXX,      C(KC_X),      C(KC_C),      C(KC_V),          XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX,           XXXXXXX,      KC_RALT,        XXXXXXX,
+    KC_PGDN,      KC_LEFT,      KC_DOWN,      KC_RGHT,          KC_PGDN,          XXXXXXX, XXXXXXX, XXXXXXX,         KC_RSFT,           KC_RCTL,      KC_LALT,        XXXXXXX,
+    XXXXXXX,      C(KC_X),      C(KC_C),      C(KC_V),          XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX,           KC_RGUI,      KC_RALT,        XXXXXXX,
     XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_TAB,           KC_SPC,           XXXXXXX, XXXXXXX, KC_ENT,          KC_BSPC,           XXXXXXX,      XXXXXXX,        XXXXXXX
 )
 
