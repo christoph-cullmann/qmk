b5397cc5a4c7db50002af4c0ad26adaa94e484da
Christoph Cullmann <cullmann@kde.org>
2022-10-24 16:07:09 +0200

    even better esc place

--- keymap.c
+++ keymap.c
@@ -98,28 +98,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX,        C(KC_X),      C(KC_C),      C(KC_V),          XXXXXXX,          KC_LCBR,         KC_AMPR,           KC_ASTR,         KC_LPRN,        KC_RCBR,
     XXXXXXX,        KC_LALT,      KC_LCTL,      KC_LSFT,          XXXXXXX,          KC_PLUS,         KC_DLR,            KC_PERC,         KC_CIRC,        KC_COLN,
     XXXXXXX,        KC_RALT,      KC_LGUI,      XXXXXXX,          XXXXXXX,          KC_PIPE,         KC_EXLM,           KC_AT,           KC_HASH,        KC_TILD,
-                                                KC_ESC,           KC_SPC,           KC_UNDS,         KC_RPRN
+                                                KC_TAB,           KC_SPC,           KC_UNDS,         KC_RPRN
 ),
 
 [_NUM] = LAYOUT_cullmann(
     XXXXXXX,        C(KC_X),      C(KC_C),      C(KC_V),          XXXXXXX,          KC_LBRC,         KC_7,              KC_8,            KC_9,           KC_RBRC,
     XXXXXXX,        KC_LALT,      KC_LCTL,      KC_LSFT,          XXXXXXX,          KC_EQL,          KC_4,              KC_5,            KC_6,           KC_SCLN,
     XXXXXXX,        KC_RALT,      KC_LGUI,      XXXXXXX,          XXXXXXX,          KC_BSLS,         KC_1,              KC_2,            KC_3,           KC_GRV,
-                                                KC_ESC,           KC_SPC,           KC_MINS,         KC_0
+                                                KC_TAB,           KC_SPC,           KC_MINS,         KC_0
 ),
 
 [_FN] = LAYOUT_cullmann(
     KC_F12,         KC_F7,        KC_F8,        KC_F9,            KC_PSCR,          QK_BOOT,         XXXXXXX,           XXXXXXX,         XXXXXXX,        XXXXXXX,
     KC_F11,         KC_F4,        KC_F5,        KC_F6,            KC_DEL,           XXXXXXX,         KC_RSFT,           KC_RCTL,         KC_LALT,        XXXXXXX,
     KC_F10,         KC_F1,        KC_F2,        KC_F3,            KC_INS,           XXXXXXX,         XXXXXXX,           KC_RGUI,         KC_RALT,        XXXXXXX,
-                                                KC_TAB,           KC_SPC,           KC_ENT,          KC_BSPC
+                                                KC_TAB,           KC_SPC,           KC_ESC,          KC_BSPC
 ),
 
 [_NAV] = LAYOUT_cullmann(
     KC_PGUP,        KC_HOME,      KC_UP,        KC_END,           KC_PGUP,          QK_BOOT,         XXXXXXX,           XXXXXXX,         XXXXXXX,        XXXXXXX,
     KC_PGDN,        KC_LEFT,      KC_DOWN,      KC_RGHT,          KC_PGDN,          XXXXXXX,         KC_RSFT,           KC_RCTL,         KC_LALT,        XXXXXXX,
     XXXXXXX,        XXXXXXX,      XXXXXXX,      XXXXXXX,          XXXXXXX,          XXXXXXX,         XXXXXXX,           KC_RGUI,         KC_RALT,        XXXXXXX,
-                                                KC_TAB,           KC_SPC,           KC_ENT,          KC_BSPC
+                                                KC_TAB,           KC_SPC,           KC_ESC,          KC_BSPC
 )
 
 };
