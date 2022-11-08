ab92685a82c3b063eecfd2eb933dda8256f72523
Christoph Cullmann <cullmann@kde.org>
2022-11-08 19:28:16 +0100

    ferris sweep has no audio or lighting

--- ferris/config.h
+++ ferris/config.h
@@ -37,41 +37,6 @@
 #define PERMISSIVE_HOLD
 
 //
-// audio configuration
-//
-
-// fix startup sound
-#define AUDIO_INIT_DELAY
-
-// normal startup sound
-#define STARTUP_SONG SONG(PLANCK_SOUND)
-
-// no music mode used
-#define NO_MUSIC_MODE
-
-//
-// underglow configuration
-//
-
-// no lights if we sleep
-#define RGBLIGHT_SLEEP
-
-// more modes for lights
-#define RGBLIGHT_EFFECT_ALTERNATING
-#define RGBLIGHT_EFFECT_BREATHING
-#define RGBLIGHT_EFFECT_CHRISTMAS
-#define RGBLIGHT_EFFECT_KNIGHT
-#define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#define RGBLIGHT_EFFECT_RGB_TEST
-#define RGBLIGHT_EFFECT_SNAKE
-#define RGBLIGHT_EFFECT_STATIC_GRADIENT
-#define RGBLIGHT_EFFECT_TWINKLE
-
-// we want to have different backlight per layers
-#define RGBLIGHT_LAYERS
-
-//
 // remove some features we don't need
 //
 
