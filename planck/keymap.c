1dfd70ec65bf4c0b8ad17f369de9005455bcc23d
Christoph Cullmann <cullmann@kde.org>
2022-12-27 19:05:54 +0100

    improve layer switching

--- planck/keymap.c
+++ planck/keymap.c
@@ -89,36 +89,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_cullmann(
     KC_Q,           KC_W,         KC_F,         KC_P,             KC_B,             KC_J,            KC_L,              KC_U,            KC_Y,           KC_QUOT,
-    LT(_NUM, KC_A), LALT_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T),     KC_G,             KC_M,            RSFT_T(KC_N),      RCTL_T(KC_E),    LALT_T(KC_I),   LT(_NAV, KC_O),
-    LT(_SYM, KC_Z), RALT_T(KC_X), LGUI_T(KC_C), KC_D,             KC_V,             KC_K,            KC_H,              RGUI_T(KC_COMM), RALT_T(KC_DOT), LT(_FN, KC_SLSH),
-                                                KC_TAB,           KC_SPC,           KC_ENT,          KC_BSPC
+    LT(_NUM, KC_A), LALT_T(KC_R), LCTL_T(KC_S), LT(_SYM, KC_T),   KC_G,             KC_M,            LT(_FN, KC_N),     RCTL_T(KC_E),    LALT_T(KC_I),   LT(_NAV, KC_O),
+    KC_Z,           KC_X,         LGUI_T(KC_C), RALT_T(KC_D),     KC_V,             KC_K,            RALT_T(KC_H),      RGUI_T(KC_COMM), KC_DOT,         KC_SLSH,
+                                                KC_TAB,           LSFT_T(KC_SPC),   RSFT_T(KC_ENT),  KC_BSPC
 ),
 
 [_SYM] = LAYOUT_cullmann(
     XXXXXXX,        C(KC_X),      C(KC_C),      C(KC_V),          XXXXXXX,          KC_LCBR,         KC_AMPR,           KC_ASTR,         KC_LPRN,        KC_RCBR,
-    XXXXXXX,        KC_LALT,      KC_LCTL,      KC_LSFT,          XXXXXXX,          KC_PLUS,         KC_DLR,            KC_PERC,         KC_CIRC,        KC_COLN,
-    XXXXXXX,        KC_RALT,      KC_LGUI,      XXXXXXX,          XXXXXXX,          KC_PIPE,         KC_EXLM,           KC_AT,           KC_HASH,        KC_TILD,
+    KC_LSFT,        KC_LALT,      KC_LCTL,      XXXXXXX,          XXXXXXX,          KC_PLUS,         KC_DLR,            KC_PERC,         KC_CIRC,        KC_COLN,
+    XXXXXXX,        XXXXXXX,      KC_LGUI,      KC_RALT,          XXXXXXX,          KC_PIPE,         KC_EXLM,           KC_AT,           KC_HASH,        KC_TILD,
                                                 KC_TAB,           KC_SPC,           KC_UNDS,         KC_RPRN
 ),
 
 [_NUM] = LAYOUT_cullmann(
     XXXXXXX,        C(KC_X),      C(KC_C),      C(KC_V),          XXXXXXX,          KC_LBRC,         KC_7,              KC_8,            KC_9,           KC_RBRC,
     XXXXXXX,        KC_LALT,      KC_LCTL,      KC_LSFT,          XXXXXXX,          KC_EQL,          KC_4,              KC_5,            KC_6,           KC_SCLN,
-    XXXXXXX,        KC_RALT,      KC_LGUI,      XXXXXXX,          XXXXXXX,          KC_BSLS,         KC_1,              KC_2,            KC_3,           KC_GRV,
+    XXXXXXX,        XXXXXXX,      KC_LGUI,      KC_RALT,          XXXXXXX,          KC_BSLS,         KC_1,              KC_2,            KC_3,           KC_GRV,
                                                 KC_TAB,           KC_SPC,           KC_MINS,         KC_0
 ),
 
 [_FN] = LAYOUT_cullmann(
     KC_F12,         KC_F7,        KC_F8,        KC_F9,            KC_PSCR,          QK_BOOT,         XXXXXXX,           XXXXXXX,         XXXXXXX,        XXXXXXX,
-    KC_F11,         KC_F4,        KC_F5,        KC_F6,            KC_DEL,           XXXXXXX,         KC_RSFT,           KC_RCTL,         KC_LALT,        XXXXXXX,
-    KC_F10,         KC_F1,        KC_F2,        KC_F3,            KC_INS,           XXXXXXX,         XXXXXXX,           KC_RGUI,         KC_RALT,        XXXXXXX,
+    KC_F11,         KC_F4,        KC_F5,        KC_F6,            KC_DEL,           XXXXXXX,         XXXXXXX,           KC_RCTL,         KC_LALT,        KC_RSFT,
+    KC_F10,         KC_F1,        KC_F2,        KC_F3,            KC_INS,           XXXXXXX,         KC_RALT,           KC_RGUI,         XXXXXXX,        XXXXXXX,
                                                 KC_TAB,           KC_SPC,           KC_ESC,          KC_BSPC
 ),
 
 [_NAV] = LAYOUT_cullmann(
     XXXXXXX,        KC_PGUP,      KC_UP,        KC_PGDN,          XXXXXXX,          QK_BOOT,         XXXXXXX,           XXXXXXX,         XXXXXXX,        XXXXXXX,
     KC_HOME,        KC_LEFT,      KC_DOWN,      KC_RGHT,          KC_END,           XXXXXXX,         KC_RSFT,           KC_RCTL,         KC_LALT,        XXXXXXX,
-    XXXXXXX,        XXXXXXX,      XXXXXXX,      XXXXXXX,          XXXXXXX,          XXXXXXX,         XXXXXXX,           KC_RGUI,         KC_RALT,        XXXXXXX,
+    XXXXXXX,        XXXXXXX,      XXXXXXX,      XXXXXXX,          XXXXXXX,          XXXXXXX,         KC_RALT,           KC_RGUI,         XXXXXXX,        XXXXXXX,
                                                 KC_TAB,           KC_SPC,           KC_ESC,          KC_BSPC
 )
 
