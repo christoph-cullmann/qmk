af5e252f24804b7de1064a9fade32094c47cc279
Christoph Cullmann <cullmann@kde.org>
2023-01-06 18:56:00 +0100

    canary

--- planck/keymap.c
+++ planck/keymap.c
@@ -88,9 +88,9 @@ enum planck_layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_cullmann(
-    KC_Q,           KC_W,         KC_F,         KC_P,             KC_B,             KC_J,            KC_L,              KC_U,            KC_Y,           KC_QUOT,
-    LT(_NUM, KC_A), LALT_T(KC_R), LCTL_T(KC_S), LT(_SYM, KC_T),   KC_G,             KC_M,            LT(_FN, KC_N),     RCTL_T(KC_E),    LALT_T(KC_I),   LT(_NAV, KC_O),
-    KC_Z,           KC_X,         LGUI_T(KC_C), RALT_T(KC_D),     KC_V,             KC_K,            RALT_T(KC_H),      RGUI_T(KC_COMM), KC_DOT,         KC_SLSH,
+    KC_W,           KC_L,         KC_Y,         KC_P,             KC_B,             KC_Z,            KC_F,              KC_O,            KC_U,           KC_QUOT,
+    LT(_NUM, KC_C), LALT_T(KC_R), LCTL_T(KC_S), LT(_SYM, KC_T),   KC_G,             KC_M,            LT(_FN, KC_N),     RCTL_T(KC_E),    LALT_T(KC_I),   LT(_NAV, KC_A),
+    KC_Q,           KC_J,         LGUI_T(KC_V), RALT_T(KC_D),     KC_K,             KC_X,            RALT_T(KC_H),      RGUI_T(KC_SLSH), KC_COMM,        KC_DOT,
                                                 KC_TAB,           LSFT_T(KC_SPC),   RSFT_T(KC_ENT),  KC_BSPC
 ),
 
