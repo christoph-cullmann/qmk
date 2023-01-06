3ddeef8fcda5c302b053fe11567421ed6c443c52
Christoph Cullmann <cullmann@kde.org>
2023-01-06 12:58:21 +0100

    use default tapping term

--- ferris/config.h
+++ ferris/config.h
@@ -23,8 +23,8 @@
 // settings for home row modifiers
 // details see https://precondition.github.io/home-row-mods
 
-// bit lower the default from 200
-#define TAPPING_TERM 180
+// the default of 200
+#define TAPPING_TERM 200
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
