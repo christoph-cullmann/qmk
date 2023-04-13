7f1fe0dd805d199795d7092903a43537b47346b0
Christoph Cullmann <cullmann@kde.org>
2023-04-13 15:29:43 +0200

    try my keymap

--- totem/keymaps/default/rules.mk
+++ totem/keymaps/default/rules.mk
@@ -1,1 +1,6 @@
-DYNAMIC_MACRO_ENABLE = yes
+# less debugging features
+COMMAND_ENABLE = no
+CONSOLE_ENABLE = no
+
+# per key debounce:  https://github.com/qmk/qmk_firmware/blob/master/docs/feature_debounce_type.md
+DEBOUNCE_TYPE = sym_defer_pk
