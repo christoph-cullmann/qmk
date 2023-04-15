eea38ca33a1a3d57f0deeb334ed0aecc0dc6e4ea
Christoph Cullmann <cullmann@kde.org>
2023-04-15 21:21:30 +0200

    better debounce

--- totem/keymaps/default/rules.mk
+++ totem/keymaps/default/rules.mk
@@ -4,3 +4,7 @@ CONSOLE_ENABLE = no
 
 # Enable N-Key Rollover
 NKRO_ENABLE = yes
+
+# more responsive debouncing: https://michael.stapelberg.ch/posts/2021-05-08-keyboard-input-latency-qmk-kinesis/
+# sym_eager_pk does do some key chatter
+DEBOUNCE_TYPE = asym_eager_defer_pk
