787f91a3da14734a5ce95ff88339f1a9829d9aba
Christoph Cullmann <cullmann@kde.org>
2023-04-15 19:09:24 +0200

    better one shots

--- totem/keymaps/default/config.h
+++ totem/keymaps/default/config.h
@@ -1,7 +1,17 @@
 #pragma once
 
-/* The way how "handedness" is decided (which half is which),
-   see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
-   for more options.
-*/
+/**
+ * The way how "handedness" is decided (which half is which),
+ * see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness for more options.
+ */
 #define MASTER_LEFT
+
+/**
+ * Tapping this number of times holds the key until tapped once again.
+ */
+#define ONESHOT_TAP_TOGGLE 0
+
+/**
+ * Time (in ms) before the one shot key is released.
+ */
+#define ONESHOT_TIMEOUT 3000
--- totem/keymaps/default/rules.mk
+++ totem/keymaps/default/rules.mk
@@ -1,6 +1,3 @@
 # less debugging features
 COMMAND_ENABLE = no
 CONSOLE_ENABLE = no
-
-# per key debounce:  https://github.com/qmk/qmk_firmware/blob/master/docs/feature_debounce_type.md
-DEBOUNCE_TYPE = sym_defer_pk
