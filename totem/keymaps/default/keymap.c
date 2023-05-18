7f0226e07fe73fba49998e6295d90fd7c94a77c3
Christoph Cullmann <cullmann@kde.org>
2023-05-18 18:16:43 +0200

    swap ralt and the gui button
    ralt more important for eurkey layout

--- totem/keymaps/default/keymap.c
+++ totem/keymaps/default/keymap.c
@@ -48,28 +48,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_SYM] = LAYOUT(
     XXXXXXX,        C(KC_X),      C(KC_C),      C(KC_V),          XXXXXXX,          KC_LCBR,         KC_AMPR,           KC_ASTR,         KC_LPRN,        KC_RCBR,
-   OSM(MOD_LGUI),  OSM(MOD_LALT),  OSM(MOD_LCTL),  OSM(MOD_LSFT),    OSM(MOD_RALT),        KC_PLUS,         KC_DLR,            KC_PERC,         KC_CIRC,        KC_COLN,
+   OSM(MOD_RALT),  OSM(MOD_LALT),  OSM(MOD_LCTL),  OSM(MOD_LSFT),    OSM(MOD_LGUI),        KC_PLUS,         KC_DLR,            KC_PERC,         KC_CIRC,        KC_COLN,
    KC_LSFT,   XXXXXXX,        XXXXXXX,      KC_LGUI,      KC_RALT,          XXXXXXX,          KC_PIPE,         KC_EXLM,           KC_AT,           KC_HASH,        KC_TILD,KC_RSFT,
                                       _______,           _______,           _______,           KC_UNDS,         KC_LPRN, KC_RPRN
 ),
 
 [_NUM] = LAYOUT(
     XXXXXXX,        C(KC_X),      C(KC_C),      C(KC_V),          XXXXXXX,          KC_LBRC,         KC_7,              KC_8,            KC_9,           KC_RBRC,
-    OSM(MOD_LGUI),  OSM(MOD_LALT),  OSM(MOD_LCTL),  OSM(MOD_LSFT),    OSM(MOD_RALT),        KC_EQL,          KC_4,              KC_5,            KC_6,           KC_SCLN,
+    OSM(MOD_RALT),  OSM(MOD_LALT),  OSM(MOD_LCTL),  OSM(MOD_LSFT),    OSM(MOD_LGUI),        KC_EQL,          KC_4,              KC_5,            KC_6,           KC_SCLN,
     KC_LSFT,  XXXXXXX,        XXXXXXX,      KC_LGUI,      KC_RALT,          XXXXXXX,          KC_BSLS,         KC_1,              KC_2,            KC_3,           KC_GRV,KC_RSFT,
                                     _______,              _______,           _______,           KC_MINS,         KC_0, KC_DOT
 ),
 
 [_FN] = LAYOUT(
     KC_F12,         KC_F7,        KC_F8,        KC_F9,            KC_PSCR,          QK_BOOT,         XXXXXXX,           XXXXXXX,         XXXXXXX,        XXXXXXX,
-    KC_F11,         KC_F4,        KC_F5,        KC_F6,            KC_DEL,           OSM(MOD_RALT),         OSM(MOD_RSFT),           OSM(MOD_RCTL),         OSM(MOD_LALT),        OSM(MOD_RGUI),
+    KC_F11,         KC_F4,        KC_F5,        KC_F6,            KC_DEL,           OSM(MOD_RGUI),         OSM(MOD_RSFT),           OSM(MOD_RCTL),         OSM(MOD_LALT),        OSM(MOD_RALT),
    KC_LSFT,   KC_F10,         KC_F1,        KC_F2,        KC_F3,            KC_INS,           XXXXXXX,         KC_RALT,           KC_RGUI,         XXXXXXX,        XXXXXXX,KC_RSFT,
                                      KC_TAB,            KC_ENT,           KC_ESC,           _______,          _______, _______
 ),
 
 [_NAV] = LAYOUT(
     XXXXXXX,        KC_PGUP,      KC_UP,        KC_PGDN,          XXXXXXX,          QK_BOOT,         XXXXXXX,           XXXXXXX,         XXXXXXX,        XXXXXXX,
-    KC_HOME,        KC_LEFT,      KC_DOWN,      KC_RGHT,          KC_END,            OSM(MOD_RALT),         OSM(MOD_RSFT),           OSM(MOD_RCTL),         OSM(MOD_LALT),        OSM(MOD_RGUI),
+    KC_HOME,        KC_LEFT,      KC_DOWN,      KC_RGHT,          KC_END,            OSM(MOD_RGUI),         OSM(MOD_RSFT),           OSM(MOD_RCTL),         OSM(MOD_LALT),        OSM(MOD_RALT),
   KC_LSFT,    XXXXXXX,        XXXXXXX,      XXXXXXX,      XXXXXXX,          XXXXXXX,          XXXXXXX,         KC_RALT,           KC_RGUI,         XXXXXXX,        XXXXXXX,KC_RSFT,
                                      KC_TAB,            KC_ENT,           KC_ESC,             _______,          _______, _______
 )
