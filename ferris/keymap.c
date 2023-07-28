c1f706e759c698bcdaa2a7e080a071dded88d106
Christoph Cullmann <cullmann@kde.org>
2023-07-28 12:50:00 +0200

    no one shot modifiers

--- ferris/keymap.c
+++ ferris/keymap.c
@@ -48,28 +48,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_SYM] = LAYOUT(
     XXXXXXX,        C(KC_X),      C(KC_C),      C(KC_V),          XXXXXXX,          KC_LCBR,         KC_AMPR,           KC_ASTR,         KC_LPRN,        KC_RCBR,
-   OSM(MOD_RALT),  OSM(MOD_LALT),  OSM(MOD_LCTL),  OSM(MOD_LSFT),    OSM(MOD_LGUI),        KC_PLUS,         KC_DLR,            KC_PERC,         KC_CIRC,        KC_COLN,
+   KC_RALT,  KC_LALT,  KC_LCTL,  KC_LSFT,    KC_LGUI,        KC_PLUS,         KC_DLR,            KC_PERC,         KC_CIRC,        KC_COLN,
    XXXXXXX,        XXXXXXX,      XXXXXXX,      XXXXXXX,          XXXXXXX,          KC_PIPE,         KC_EXLM,           KC_AT,           KC_HASH,        KC_TILD,
                                       _______,           _______,           KC_UNDS,         KC_RPRN
 ),
 
 [_NUM] = LAYOUT(
     XXXXXXX,        C(KC_X),      C(KC_C),      C(KC_V),          XXXXXXX,          KC_LBRC,         KC_7,              KC_8,            KC_9,           KC_RBRC,
-    OSM(MOD_RALT),  OSM(MOD_LALT),  OSM(MOD_LCTL),  OSM(MOD_LSFT),    OSM(MOD_LGUI),        KC_EQL,          KC_4,              KC_5,            KC_6,           KC_SCLN,
+    KC_RALT,  KC_LALT,  KC_LCTL,  KC_LSFT,    KC_LGUI,        KC_EQL,          KC_4,              KC_5,            KC_6,           KC_SCLN,
     XXXXXXX,        XXXXXXX,      XXXXXXX,      XXXXXXX,          XXXXXXX,          KC_BSLS,         KC_1,              KC_2,            KC_3,           KC_GRV,
                                               _______,           _______,           KC_MINS,         KC_0
 ),
 
 [_FN] = LAYOUT(
     KC_F12,         KC_F7,        KC_F8,        KC_F9,            KC_PSCR,          QK_BOOT,         XXXXXXX,           XXXXXXX,         XXXXXXX,        XXXXXXX,
-    KC_F11,         KC_F4,        KC_F5,        KC_F6,            KC_DEL,           OSM(MOD_RGUI),         OSM(MOD_RSFT),           OSM(MOD_RCTL),         OSM(MOD_LALT),        OSM(MOD_RALT),
+    KC_F11,         KC_F4,        KC_F5,        KC_F6,            KC_DEL,           KC_RGUI,         KC_RSFT,           KC_RCTL,         KC_LALT,        KC_RALT,
    KC_F10,         KC_F1,        KC_F2,        KC_F3,            KC_INS,           XXXXXXX,         XXXXXXX,           XXXXXXX,         XXXXXXX,        XXXXXXX,
                                             KC_BSPC,           KC_ESC,           _______,          _______
 ),
 
 [_NAV] = LAYOUT(
     XXXXXXX,        KC_PGUP,      KC_UP,        KC_PGDN,          XXXXXXX,          QK_BOOT,         XXXXXXX,           XXXXXXX,         XXXXXXX,        XXXXXXX,
-    KC_HOME,        KC_LEFT,      KC_DOWN,      KC_RGHT,          KC_END,            OSM(MOD_RGUI),         OSM(MOD_RSFT),           OSM(MOD_RCTL),         OSM(MOD_LALT),        OSM(MOD_RALT),
+    KC_HOME,        KC_LEFT,      KC_DOWN,      KC_RGHT,          KC_END,            KC_RGUI,         KC_RSFT,           KC_RCTL,         KC_LALT,        KC_RALT,
   XXXXXXX,        XXXXXXX,      XXXXXXX,      XXXXXXX,          XXXXXXX,          XXXXXXX,         XXXXXXX,           XXXXXXX,         XXXXXXX,        XXXXXXX,
                                       KC_TAB,           KC_ENT,             _______,          _______
 )
