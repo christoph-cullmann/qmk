c3bbda01099b4d7e1973924dd58e043eaaa6d663
Christoph Cullmann <cullmann@kde.org>
2023-08-10 16:02:32 +0200

    use fastest version

--- ferris/rules.mk
+++ ferris/rules.mk
@@ -15,8 +15,7 @@ GRAVE_ESC_ENABLE = no
 MAGIC_ENABLE = no
 
 # more responsive debouncing: https://michael.stapelberg.ch/posts/2021-05-08-keyboard-input-latency-qmk-kinesis/
-# sym_eager_pk does do some key chatter
-DEBOUNCE_TYPE = asym_eager_defer_pk
+DEBOUNCE_TYPE = sym_eager_pk
 
 # build
 # qmk compile -kb ferris/sweep -km christoph-cullmann
