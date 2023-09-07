8f49b471d8c3425e6097958ba4c9de46e835a51f
Christoph Cullmann <cullmann@kde.org>
2023-09-07 19:08:59 +0200

    keep normal debounce

--- totem/keymaps/default/rules.mk
+++ totem/keymaps/default/rules.mk
@@ -11,9 +11,6 @@ SPACE_CADET_ENABLE = no
 GRAVE_ESC_ENABLE = no
 MAGIC_ENABLE = no
 
-# more responsive debouncing: https://michael.stapelberg.ch/posts/2021-05-08-keyboard-input-latency-qmk-kinesis/
-DEBOUNCE_TYPE = asym_eager_defer_pk
-
 # add achordion to improve home row modifiers
 SRC += ../../../common/achordion.c
 
