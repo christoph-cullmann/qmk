835a4e7bfbc26868f65e077a39da409a65ef6885
Christoph Cullmann <cullmann@kde.org>
2023-09-18 09:58:32 +0200

    higher TAPPING_TERM

--- common/config.h
+++ common/config.h
@@ -30,7 +30,7 @@
 // details see https://precondition.github.io/home-row-mods
 
 // the default of 200
-#define TAPPING_TERM 175
+#define TAPPING_TERM 200
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define QUICK_TAP_TERM 0
