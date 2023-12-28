bfa9397f687bbb5fbfa89d8546c965a7ebc9be67
Christoph Cullmann <cullmann@kde.org>
2023-12-28 14:18:59 +0100

    fix indentation

--- common/keymap.h
+++ common/keymap.h
@@ -28,45 +28,45 @@ enum my_layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_BASE] = LAYOUT(
-    KC_W,         KC_L,         KC_Y,         KC_P,         KC_B,         KC_Z,         KC_F,         OSL(_MAGIC),     KC_U,         KC_QUOT,
-    KC_C,         LALT_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T), LGUI_T(KC_G), RGUI_T(KC_M), RSFT_T(KC_N), RCTL_T(KC_O),    LALT_T(KC_I), KC_A,
-    KC_Q,         KC_J,         LSFT_T(KC_V), RALT_T(KC_D), KC_K,         KC_X,         RALT_T(KC_H), RSFT_T(KC_SLSH), KC_COMM,      KC_DOT,
-                                              MO(_SYM),     LT(_NUM, KC_SPC),       LT(_NAV, KC_E),         MO(_FN)
+    KC_W,         KC_L,         KC_Y,         KC_P,         KC_B,             KC_Z,           KC_F,         OSL(_MAGIC),     KC_U,         KC_QUOT,
+    KC_C,         LALT_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T), LGUI_T(KC_G),     RGUI_T(KC_M),   RSFT_T(KC_N), RCTL_T(KC_O),    LALT_T(KC_I), KC_A,
+    KC_Q,         KC_J,         LSFT_T(KC_V), RALT_T(KC_D), KC_K,             KC_X,           RALT_T(KC_H), RSFT_T(KC_SLSH), KC_COMM,      KC_DOT,
+                                              MO(_SYM),     LT(_NUM, KC_SPC), LT(_NAV, KC_E), MO(_FN)
 ),
 
 [_NUM] = LAYOUT(
-    TO(_BASE),    C(KC_X),      C(KC_C),      C(KC_V),      XXXXXXX,      KC_LBRC,      KC_7,         KC_8,            KC_9,         KC_RBRC,
-    MO(_SYM),     KC_LALT,      KC_LCTL,      KC_LSFT,      KC_LGUI,      KC_EQL,       KC_4,         KC_5,            KC_6,         KC_SCLN,
-    XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_RALT,      XXXXXXX,      KC_BSLS,      KC_1,         KC_2,            KC_3,         KC_GRV,
-                                              XXXXXXX,      XXXXXXX,      KC_MINS,      KC_0
+    TO(_BASE),    C(KC_X),      C(KC_C),      C(KC_V),      XXXXXXX,          KC_LBRC,        KC_7,         KC_8,            KC_9,         KC_RBRC,
+    MO(_SYM),     KC_LALT,      KC_LCTL,      KC_LSFT,      KC_LGUI,          KC_EQL,         KC_4,         KC_5,            KC_6,         KC_SCLN,
+    XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_RALT,      XXXXXXX,          KC_BSLS,        KC_1,         KC_2,            KC_3,         KC_GRV,
+                                              XXXXXXX,      XXXXXXX,          KC_MINS,        KC_0
 ),
 
 [_NAV] = LAYOUT(
-    XXXXXXX,      KC_PGUP,      KC_UP,        KC_PGDN,      XXXXXXX,      QK_BOOT,      XXXXXXX,      XXXXXXX,         XXXXXXX,      TO(_BASE),
-    KC_HOME,      KC_LEFT,      KC_DOWN,      KC_RGHT,      KC_END,       KC_RGUI,      KC_RSFT,      KC_RCTL,         KC_LALT,      MO(_FN),
-    XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_RALT,      XXXXXXX,         XXXXXXX,      XXXXXXX,
-                                              KC_TAB,       KC_ENT,       XXXXXXX,      XXXXXXX
+    XXXXXXX,      KC_PGUP,      KC_UP,        KC_PGDN,      XXXXXXX,          QK_BOOT,        XXXXXXX,      XXXXXXX,         XXXXXXX,      TO(_BASE),
+    KC_HOME,      KC_LEFT,      KC_DOWN,      KC_RGHT,      KC_END,           KC_RGUI,        KC_RSFT,      KC_RCTL,         KC_LALT,      MO(_FN),
+    XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,          XXXXXXX,        KC_RALT,      XXXXXXX,         XXXXXXX,      XXXXXXX,
+                                              KC_TAB,       KC_ENT,           XXXXXXX,        XXXXXXX
 ),
 
 [_SYM] = LAYOUT(
-    TO(_BASE),    C(KC_X),      C(KC_C),      C(KC_V),      XXXXXXX,      KC_LCBR,      KC_AMPR,      KC_ASTR,         KC_LPRN,      KC_RCBR,
-    XXXXXXX,      KC_LALT,      KC_LCTL,      KC_LSFT,      KC_LGUI,      KC_PLUS,      KC_DLR,       KC_PERC,         KC_CIRC,      KC_COLN,
-    XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_RALT,      XXXXXXX,      KC_PIPE,      KC_EXLM,      KC_AT,           KC_HASH,      KC_TILD,
-                                              XXXXXXX,      XXXXXXX,      KC_UNDS,      KC_RPRN
+    TO(_BASE),    C(KC_X),      C(KC_C),      C(KC_V),      XXXXXXX,          KC_LCBR,        KC_AMPR,      KC_ASTR,         KC_LPRN,      KC_RCBR,
+    XXXXXXX,      KC_LALT,      KC_LCTL,      KC_LSFT,      KC_LGUI,          KC_PLUS,        KC_DLR,       KC_PERC,         KC_CIRC,      KC_COLN,
+    XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_RALT,      XXXXXXX,          KC_PIPE,        KC_EXLM,      KC_AT,           KC_HASH,      KC_TILD,
+                                              XXXXXXX,      XXXXXXX,          KC_UNDS,        KC_RPRN
 ),
 
 [_FN] = LAYOUT(
-    KC_F12,       KC_F7,        KC_F8,        KC_F9,        KC_PSCR,      QK_BOOT,      XXXXXXX,      XXXXXXX,         XXXXXXX,      TO(_BASE),
-    KC_F11,       KC_F4,        KC_F5,        KC_F6,        KC_DEL,       KC_RGUI,      KC_RSFT,      KC_RCTL,         KC_LALT,      XXXXXXX,
-    KC_F10,       KC_F1,        KC_F2,        KC_F3,        KC_INS,       XXXXXXX,      KC_RALT,      XXXXXXX,         XXXXXXX,      XXXXXXX,
-                                              KC_BSPC,      KC_ESC,       XXXXXXX,      XXXXXXX
+    KC_F12,       KC_F7,        KC_F8,        KC_F9,        KC_PSCR,          QK_BOOT,        XXXXXXX,      XXXXXXX,         XXXXXXX,      TO(_BASE),
+    KC_F11,       KC_F4,        KC_F5,        KC_F6,        KC_DEL,           KC_RGUI,        KC_RSFT,      KC_RCTL,         KC_LALT,      XXXXXXX,
+    KC_F10,       KC_F1,        KC_F2,        KC_F3,        KC_INS,           XXXXXXX,        KC_RALT,      XXXXXXX,         XXXXXXX,      XXXXXXX,
+                                              KC_BSPC,      KC_ESC,           XXXXXXX,        XXXXXXX
 ),
 
 [_MAGIC] = LAYOUT(
-    XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_CAPS,         XXXXXXX,      XXXXXXX,
-    KC_LBRC,      KC_RBRC,      KC_LPRN,      KC_RPRN,      XXXXXXX,      XXXXXXX,      KC_LCBR,      KC_RCBR,         S(KC_COMMA),  S(KC_DOT),
-    TO(_NUM),     TO(_SYM),     XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,         TO(_FN),      TO(_NAV),
-                                              XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX
+    XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,          XXXXXXX,        XXXXXXX,      KC_CAPS,         XXXXXXX,      XXXXXXX,
+    KC_LBRC,      KC_RBRC,      KC_LPRN,      KC_RPRN,      XXXXXXX,          XXXXXXX,        KC_LCBR,      KC_RCBR,         S(KC_COMMA),  S(KC_DOT),
+    TO(_NUM),     TO(_SYM),     XXXXXXX,      XXXXXXX,      XXXXXXX,          XXXXXXX,        XXXXXXX,      XXXXXXX,         TO(_FN),      TO(_NAV),
+                                              XXXXXXX,      XXXXXXX,          XXXXXXX,        XXXXXXX
 )
 
 };
