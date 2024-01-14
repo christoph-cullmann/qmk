9599adadc35f3d3746c2c3811cd4fba05c09a305
Christoph Cullmann <cullmann@kde.org>
2024-01-14 16:44:59 +0100

    try dhorf

--- README.md
+++ README.md
@@ -1,22 +1,22 @@
 # Christoph Cullmann's Layout
 
-Layout with 34 keys, using Canary as base layer:
+Layout with 34 keys, using Dhorf as base layer:
 
-    canary-ortho
-      w l y p b  z f o u '
-      c r s t g  m n e i a
-      q j v d k  x h / , .
+    dhorf (Oxey)
+      v l h k q  j f o u ,
+      s r n t w  y c a e i
+      z x m d b  p g ' ; .
 
-    MT-QUOTES:
-      Alt: 23.47%
-      Rol: 48.11%   (In/Out: 21.19% | 26.92%)
-      One:  2.20%   (In/Out:  0.89% |  1.31%)
-      Red:  3.26%   (Bad:     0.08%)
+    SHAI:
+      Alt: 31.42%
+      Rol: 40.03%   (In/Out: 19.86% | 20.17%)
+      One:  1.36%   (In/Out:  0.18% |  1.19%)
+      Red:  3.33%   (Bad:     0.11%)
 
-      SFB: 0.94%
-      SFS: 7.80%    (Red/Alt: 3.62% | 4.17%)
+      SFB: 0.70%
+      SFS: 5.96%    (Red/Alt: 0.98% | 4.98%)
 
-      LH/RH: 42.15% | 57.85%
+      LH/RH: 48.52% | 51.48%
 
 ATM this needs to be cloned to
 
@@ -26,7 +26,7 @@ to work properly.
 
 Based on
 
-https://github.com/Apsu/Canary
+https://oxey.dev/dhorf
 
 and
 
--- common/keymap.h
+++ common/keymap.h
@@ -27,15 +27,15 @@ enum my_layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_BASE] = LAYOUT(
-    KC_W,         KC_L,         KC_Y,         KC_P,         KC_B,             KC_Z,           KC_F,         KC_O,         KC_U,         KC_QUOT,
-    RALT_T(KC_C), LALT_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T), KC_G,             KC_M,           RSFT_T(KC_N), RCTL_T(KC_E), LALT_T(KC_I), RALT_T(KC_A),
-    KC_Q,         KC_J,         KC_V,         LGUI_T(KC_D), KC_K,             KC_X,           RGUI_T(KC_H), KC_SLSH,      KC_COMM,      KC_DOT,
+    KC_V,         KC_L,         KC_H,         KC_K,         KC_Q,             KC_J,           KC_F,         KC_O,         KC_U,         KC_COMM,
+    RALT_T(KC_S), LALT_T(KC_R), LCTL_T(KC_N), LSFT_T(KC_T), KC_W,             KC_Y,           RSFT_T(KC_C), RCTL_T(KC_A), LALT_T(KC_E), RALT_T(KC_I),
+    KC_Z,         KC_X,         KC_M,         LGUI_T(KC_D), KC_B,             KC_P,           RGUI_T(KC_G), KC_QUOT,      KC_SCLN,      KC_DOT,
                                               MO(_SYM),     LT(_NUM, KC_SPC), MO(_NAV),       MO(_FN)
 ),
 
 [_NUM] = LAYOUT(
     TO(_BASE),    C(KC_X),      C(KC_C),      C(KC_V),      XXXXXXX,          KC_LBRC,        KC_7,         KC_8,         KC_9,         KC_RBRC,
-    KC_RALT,      KC_LALT,      KC_LCTL,      KC_LSFT,      XXXXXXX,          KC_EQL,         KC_4,         KC_5,         KC_6,         KC_SCLN,
+    KC_RALT,      KC_LALT,      KC_LCTL,      KC_LSFT,      XXXXXXX,          KC_EQL,         KC_4,         KC_5,         KC_6,         KC_SLSH,
     XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_LGUI,      XXXXXXX,          KC_BSLS,        KC_1,         KC_2,         KC_3,         KC_GRV,
                                               XXXXXXX,      XXXXXXX,          KC_MINS,        KC_0
 ),
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_SYM] = LAYOUT(
     TO(_BASE),    C(KC_X),      C(KC_C),      C(KC_V),      XXXXXXX,          KC_LCBR,        KC_AMPR,      KC_ASTR,      KC_LPRN,      KC_RCBR,
-    KC_RALT,      KC_LALT,      KC_LCTL,      KC_LSFT,      XXXXXXX,          KC_PLUS,        KC_DLR,       KC_PERC,      KC_CIRC,      KC_COLN,
+    KC_RALT,      KC_LALT,      KC_LCTL,      KC_LSFT,      XXXXXXX,          KC_PLUS,        KC_DLR,       KC_PERC,      KC_CIRC,      KC_QUES,
     XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_LGUI,      XXXXXXX,          KC_PIPE,        KC_EXLM,      KC_AT,        KC_HASH,      KC_TILD,
                                               XXXXXXX,      XXXXXXX,          KC_UNDS,        KC_RPRN
 ),
