9720844cb1d15920f6b2c8cd931e2891ea91fe84
Christoph Cullmann <christoph@cullmann.io>
2024-06-15 14:48:34 +0200

    no q8 around any longer

--- q8/rules.mk
+++
@@ -1,6 +0,0 @@
-# include common settings
-include /home/cullmann/data/qmk/christoph-cullmann/common/rules.mk
-
-# build
-# qmk compile -kb keychron/q8/iso_encoder -km christoph-cullmann
-# qmk flash -kb keychron/q8/iso_encoder -km christoph-cullmann
