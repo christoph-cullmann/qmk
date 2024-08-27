834efbc6c61352134500e485827fb12d64585b64
Christoph Cullmann <christoph@cullmann.io>
2024-08-27 18:18:57 +0200

    fix half config at home

--- crkbd/rules.mk
+++ crkbd/rules.mk
@@ -3,4 +3,5 @@ include /home/cullmann/data/qmk/christoph-cullmann/common/rules.mk
 
 # build
 # qmk compile -kb crkbd -km christoph-cullmann
-# qmk flash -kb crkbd -km christoph-cullmann
+# qmk flash -kb crkbd -km christoph-cullmann -bl avrdude-split-left
+# qmk flash -kb crkbd -km christoph-cullmann -bl avrdude-split-right
