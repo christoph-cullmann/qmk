9a5daf3e45f5b5ec7406797a4e24eea778b1705a
Christoph Cullmann <christoph@cullmann.io>
2024-09-23 01:25:37 +0200

    don't do the OLED stuff for Planck

--- keyboards/crkbd/keymaps/christoph-cullmann/rules.mk
+++ keyboards/crkbd/keymaps/christoph-cullmann/rules.mk
@@ -1,3 +1,6 @@
+# set flags we check in common settings
+OLED_ENABLE = yes
+
 # include common settings
 ROOT_DIR := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
 include ${ROOT_DIR}../../../../rules.mk
--- rules.mk
+++ rules.mk
@@ -13,9 +13,11 @@ SPACE_CADET_ENABLE = no
 # add achordion to improve home row modifiers
 SRC += features/achordion.c
 
-# add bongocat & luna for OLED
-SRC += features/oled_bongocat.c
-SRC += features/oled_luna.c
+# add bongocat & luna for OLEDs
+ifeq ($(strip $(OLED_ENABLE)), yes)
+    SRC += features/oled_bongocat.c
+    SRC += features/oled_luna.c
+endif
 
 # good optimizations
 LTO_ENABLE = yes
