421578ddf65a9822e14eba39fce881895251947a
Christoph Cullmann <cullmann@kde.org>
2024-10-01 18:14:17 +0200

    back to 32 keys

--- common/keymap.h
+++ common/keymap.h
@@ -19,35 +19,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     C(KC_X),      KC_B,         KC_F,         KC_L,         KC_K,         KC_Q,            KC_P,           KC_G,         KC_O,         KC_U,         KC_DOT,       KC_PSCR,
     C(KC_C),      RALT_T(KC_N), LALT_T(KC_S), LCTL_T(KC_H), LSFT_T(KC_T), KC_M,            KC_Y,           RSFT_T(KC_C), RCTL_T(KC_A), LALT_T(KC_E), RALT_T(KC_I), XXXXXXX,
     C(KC_V),      KC_X,         KC_V,         KC_J,         LGUI_T(KC_D), KC_Z,            KC_QUOT,        RGUI_T(KC_W), KC_SCLN,      KC_SLSH,      KC_COMM,      XXXXXXX,
-                                              MO(_SYM),     KC_R,         MO(_NUM),        MO(_NAV),       KC_SPC,       MO(_FN)
+                                              XXXXXXX,      MO(_SYM),     LT(_NUM, KC_R),  LT(_NAV, KC_SPC), MO(_FN), XXXXXXX
 ),
 
 [_NUM] = LAYOUT(
     _______,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      QK_RBT,          KC_LBRC,        KC_7,         KC_8,         KC_9,         KC_RBRC,      _______,
     _______,      KC_RALT,      KC_LALT,      KC_LCTL,      KC_LSFT,      XXXXXXX,         KC_EQL,         KC_4,         KC_5,         KC_6,         KC_SLSH,      _______,
     _______,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_LGUI,      XXXXXXX,         KC_BSLS,        KC_1,         KC_2,         KC_3,         KC_GRV,       _______,
-                                              XXXXXXX,      XXXXXXX,      XXXXXXX,         KC_MINS,        KC_0,         KC_DOT
+                                              XXXXXXX,      XXXXXXX,      XXXXXXX,         KC_MINS,        KC_0,         XXXXXXX
 ),
 
 [_NAV] = LAYOUT(
     _______,      XXXXXXX,      KC_PGUP,      KC_UP,        KC_PGDN,      XXXXXXX,         QK_RBT,         XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      _______,
     _______,      KC_HOME,      KC_LEFT,      KC_DOWN,      KC_RGHT,      KC_END,          XXXXXXX,        KC_RSFT,      KC_RCTL,      KC_LALT,      KC_RALT,      _______,
     _______,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,         XXXXXXX,        KC_RGUI,      XXXXXXX,      XXXXXXX,      XXXXXXX,      _______,
-                                              KC_TAB,       KC_ENT,       XXXXXXX,         XXXXXXX,        XXXXXXX,      XXXXXXX
+                                              XXXXXXX,       KC_TAB,       KC_ENT,         XXXXXXX,        XXXXXXX,      XXXXXXX
 ),
 
 [_SYM] = LAYOUT(
     _______,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      QK_BOOT,         KC_LCBR,        KC_AMPR,      KC_ASTR,      KC_LPRN,      KC_RCBR,      _______,
     _______,      KC_RALT,      KC_LALT,      KC_LCTL,      KC_LSFT,      XXXXXXX,         KC_PLUS,        KC_DLR,       KC_PERC,      KC_CIRC,      KC_QUES,      _______,
     _______,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_LGUI,      XXXXXXX,         KC_PIPE,        KC_EXLM,      KC_AT,        KC_HASH,      KC_TILD,      _______,
-                                              XXXXXXX,      XXXXXXX,      XXXXXXX,         KC_UNDS,        KC_LPRN,      KC_RPRN
+                                              XXXXXXX,      XXXXXXX,      XXXXXXX,         KC_UNDS,        KC_RPRN,      XXXXXXX
 ),
 
 [_FN] = LAYOUT(
     _______,      KC_F12,       KC_F7,        KC_F8,        KC_F9,        XXXXXXX,         QK_BOOT,        XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      _______,
     _______,      KC_F11,       KC_F4,        KC_F5,        KC_F6,        KC_DEL,          XXXXXXX,        KC_RSFT,      KC_RCTL,      KC_LALT,      KC_RALT,      _______,
     _______,      KC_F10,       KC_F1,        KC_F2,        KC_F3,        KC_INS,          XXXXXXX,        KC_RGUI,      XXXXXXX,      XXXXXXX,      XXXXXXX,      _______,
-                                              KC_BSPC,      KC_ESC,       XXXXXXX,         XXXXXXX,        XXXXXXX,      XXXXXXX
+                                              XXXXXXX,      KC_BSPC,       KC_ESC,         XXXXXXX,        XXXXXXX,      XXXXXXX
 )
 
 };
