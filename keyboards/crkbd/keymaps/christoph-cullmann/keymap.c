f23e38366f1a0cbb19fa01e53ac95075fa33c0be
Christoph Cullmann <christoph@cullmann.io>
2024-10-01 23:33:02 +0200

    layout with 34 keys

--- keyboards/crkbd/keymaps/christoph-cullmann/keymap.c
+++ keyboards/crkbd/keymaps/christoph-cullmann/keymap.c
@@ -14,16 +14,16 @@ static bool on_left_hand(keypos_t pos)
 // layout helper macro, we just use 42 keys
 #undef LAYOUT
 #define LAYOUT(\
-K00,   K01,   K02,   K03,   K04,   K05,   K06,   K07,   K08,   K09,   K10,   K11,\
-K12,   K13,   K14,   K15,   K16,   K17,   K18,   K19,   K20,   K21,   K22,   K23,\
-K24,   K25,   K26,   K27,   K28,   K29,   K30,   K31,   K32,   K33,   K34,   K35,\
-                     K36,   K37,   K38,   K39,   K40,   K41\
+K00,   K01,   K02,   K03,   K04,                 K05,   K06,   K07,   K08,   K09,\
+K10,   K11,   K12,   K13,   K14,                 K15,   K16,   K17,   K18,   K19,\
+K20,   K21,   K22,   K23,   K24,                 K25,   K26,   K27,   K28,   K29,\
+                     K30,   K31,                 K32,   K33\
 )\
 LAYOUT_split_3x6_3(\
-K00,   K01,   K02,   K03,   K04,   K05,   K06,   K07,   K08,   K09,   K10,   K11,\
-K12,   K13,   K14,   K15,   K16,   K17,   K18,   K19,   K20,   K21,   K22,   K23,\
-K24,   K25,   K26,   K27,   K28,   K29,   K30,   K31,   K32,   K33,   K34,   K35,\
-                     K36,   K37,   K38,   K39,   K40,   K41\
+KC_NO, K00,   K01,   K02,   K03,   K04,          K05,   K06,   K07,   K08,   K09, KC_NO,\
+KC_NO, K10,   K11,   K12,   K13,   K14,          K15,   K16,   K17,   K18,   K19, KC_NO,\
+KC_NO, K20,   K21,   K22,   K23,   K24,          K25,   K26,   K27,   K28,   K29, KC_NO,\
+                     KC_NO, K30,   K31,          K32,   K33,   KC_NO\
 )
 
 // our shared 42 keys keymap
--- keyboards/planck/keymaps/christoph-cullmann/keymap.c
+++ keyboards/planck/keymaps/christoph-cullmann/keymap.c
@@ -14,16 +14,16 @@ static bool on_left_hand(keypos_t pos)
 // layout helper macro, we just use 42 keys
 #undef LAYOUT
 #define LAYOUT(\
-K00,   K01,   K02,   K03,   K04,   K05,   K06,   K07,   K08,   K09,   K10,   K11,\
-K12,   K13,   K14,   K15,   K16,   K17,   K18,   K19,   K20,   K21,   K22,   K23,\
-K24,   K25,   K26,   K27,   K28,   K29,   K30,   K31,   K32,   K33,   K34,   K35,\
-                     K36,   K37,   K38,   K39,   K40,   K41\
+K00,   K01,   K02,   K03,   K04,                 K05,   K06,   K07,   K08,   K09,\
+K10,   K11,   K12,   K13,   K14,                 K15,   K16,   K17,   K18,   K19,\
+K20,   K21,   K22,   K23,   K24,                 K25,   K26,   K27,   K28,   K29,\
+                     K30,   K31,                 K32,   K33\
 )\
 LAYOUT_ortho_4x12(\
-K00,   K01,   K02,   K03,   K04,   K05,   K06,   K07,   K08,   K09,   K10,   K11,\
-K12,   K13,   K14,   K15,   K16,   K17,   K18,   K19,   K20,   K21,   K22,   K23,\
-K24,   K25,   K26,   K27,   K28,   K29,   K30,   K31,   K32,   K33,   K34,   K35,\
-KC_NO, KC_NO, KC_NO, K36,   K37,   K38,   K39,   K40,   K41,   KC_NO, KC_NO, KC_NO\
+K00,   K01,   K02,   K03,   K04,   KC_NO, KC_NO, K05,   K06,   K07,   K08,   K09,\
+K10,   K11,   K12,   K13,   K14,   KC_NO, KC_NO, K15,   K16,   K17,   K18,   K19,\
+K20,   K21,   K22,   K23,   K24,   KC_NO, KC_NO, K25,   K26,   K27,   K28,   K29,\
+KC_NO, KC_NO, KC_NO, K30,   K31,   KC_NO, KC_NO, K32,   K33,   KC_NO, KC_NO, KC_NO\
 )
 
 // our shared 42 keys keymap
