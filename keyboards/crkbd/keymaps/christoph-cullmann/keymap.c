edf089efd709943333ed84c7138f6198cde8e8c4
Christoph Cullmann <christoph@cullmann.io>
2024-12-20 22:41:24 +0100

    back to achordion
    better support of one hand rolls

--- keyboards/crkbd/keymaps/christoph-cullmann/keymap.c
+++ keyboards/crkbd/keymaps/christoph-cullmann/keymap.c
@@ -20,5 +20,11 @@ KC_NO, K20,   K21,   K22,   K23,   K24,          K25,   K26,   K27,   K28,   K29
                      KC_NO, K30,   K31,          K32,   K33,   KC_NO\
 )
 
+// Returns true if `pos` on the left hand of the keyboard, false if right.
+static bool on_left_hand(keypos_t pos)
+{
+    return pos.row < MATRIX_ROWS / 2;
+}
+
 // our shared 42 keys keymap
 #include "common/keymap.h"
--- rules.mk
+++ rules.mk
@@ -1,6 +1,9 @@
 # Enable N-Key Rollover
 NKRO_ENABLE = yes
 
+# add achordion to improve home row modifiers
+SRC += features/achordion.c
+
 # add bongocat & luna for OLEDs
 ifeq ($(strip $(OLED_ENABLE)), yes)
     SRC += features/oled_bongocat.c
