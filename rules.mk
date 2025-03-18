83059d4dfa397b0ef0b63c2cfbe6d3e1bded8ee0
Christoph Cullmann <christoph@cullmann.io>
2025-03-18 22:32:42 +0100

    use getreuer/tap_flow

--- rules.mk
+++ rules.mk
@@ -4,6 +4,10 @@ NKRO_ENABLE = yes
 # optimize size
 LTO_ENABLE = yes
 
+# needed for getreuer/tap_flow
+COMBOS_ENABLE = yes
+REPEAT_KEY_ENABLE = yes
+
 # add bongocat & luna for OLEDs
 ifeq ($(strip $(OLED_ENABLE)), yes)
     SRC += features/oled_bongocat.c
