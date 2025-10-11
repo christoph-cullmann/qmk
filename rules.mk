273c293ca347c6c5f3884bb0e43b94fe7d7284d5
Christoph Cullmann <christoph@cullmann.dev>
2025-10-11 18:28:15 +0200

    less features I don't use

--- rules.mk
+++ rules.mk
@@ -15,6 +15,19 @@ MOUSEKEY_ENABLE = no
 # no extrakeys disables stuff like the media keys and system volume control
 EXTRAKEY_ENABLE = no
 
+# no magic stuff to swap buttons and co.
+MAGIC_ENABLE = no
+
+# no debug console
+CONSOLE_ENABLE = no
+
+# no commands for debug and configuration
+COMMAND_ENABLE = no
+
+# my keymap doesn't need this stuff
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
+
 # add bongocat & luna for OLEDs
 ifeq ($(strip $(OLED_ENABLE)), yes)
     SRC += features/oled_bongocat.c
