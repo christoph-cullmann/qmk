e1f1473ff694349cad4870ddd9da9324d8b7b563
Christoph Cullmann <christoph@cullmann.dev>
2026-01-09 18:39:05 +0100

    remove own debounce setting, have chatter with Gateron Type R
    on the Aurora Ferris with that

--- rules.mk
+++ rules.mk
@@ -12,9 +12,6 @@ NKRO_ENABLE = yes
 # optimize size
 LTO_ENABLE = yes
 
-# use more snappy debounce algo
-DEBOUNCE_TYPE = asym_eager_defer_pk
-
 # disable some stuff we don't need
 COMBO_ENABLE = no
 COMMAND_ENABLE = no
